55e7d58dc09e630add74ce38aec0116516b53402
Vincent <mariosonic1793@gmail.com>
2025-03-28 23:48:34 -0700

    Finished the quiz
    This includes both adding the random square generator and checker, and also optimizing the isMagicSquare() function because it looked awful and bugged me whenever I looked at it. :P

--- Lo-Shu-Magic-Square.c
+++ Lo-Shu-Magic-Square.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 bool searchSquare(int square[3][3]) {
     int total = 0;
@@ -23,26 +25,13 @@ bool searchSquare(int square[3][3]) {
 }
 
 bool isMagicSquare(int square[3][3]) {
-    if (searchSquare(square)) {
-        if (square[0][0] + square[0][1] + square[0][2] == 15) {
-            if (square[1][0] + square[1][1] + square[1][2] == 15) {
-                if (square[2][0] + square[2][1] + square[2][2] == 15) {
-                    if (square[0][0] + square[1][0] + square[2][0] == 15) {
-                        if (square[0][1] + square[1][1] + square[2][1] == 15) {
-                            if (square[0][2] + square[1][2] + square[2][2] == 15) {
-                                if (square[0][0] + square[1][1] + square[2][2] == 15) {
-                                    if (square[2][0] + square[1][1] + square[0][2] == 15) {
-                                        return true;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    for (int i = 0; i < 3; i++) {
+        if ((square[i][0] + square[i][1] + square[i][2]) != 15) return false;
+        if ((square[0][i] + square[1][i] + square[2][i]) != 15) return false;
     }
-    return false;
+    if ((square[0][0] + square[1][1] + square[2][2]) != 15) return false;
+    if ((square[0][2] + square[1][1] + square[2][0]) != 15) return false;
+    return true;
 }
 
 int main() {
@@ -58,4 +47,33 @@ int main() {
     } else {
         printf("[1 2 3]\n[4 5 6] is NOT a Lo Shu Magic Square\n[7 8 9]\n\n");
     }
+
+    srand(time(0));
+    int attempts = 0;
+    int square[3][3];
+
+    do {
+        int numbers[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        
+        for (int i = 0; i < 9; i++) {
+            int j = rand() % 9;
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+
+        // Fill the square with the shuffled numbers
+        int idx = 0;
+        for (int a = 0; a < 3; a++) {
+            for (int b = 0; b < 3; b++) {
+                square[a][b] = numbers[idx++];
+            }
+        }
+        attempts++;
+    } while (!isMagicSquare(square));
+    printf("It took %d attempts to make a Lo Shu Magic Square\n", attempts);
+    printf("[%d %d %d]\n[%d %d %d] is a Lo Shu Magic Square\n[%d %d %d]", 
+        square[0][0], square[0][1], square[0][2],
+        square[1][0], square[1][1], square[1][2],
+        square[2][0], square[2][1], square[2][2]);
 }
